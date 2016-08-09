2ed796fb3c9abcb907e22c2c32e363b6eed1d63e
Gayathri Mohan <gayathrimohan@qburst.com>
2016-08-09 09:17:12 +0530

    second commit

--- func.js
+++ func.js
@@ -4,16 +4,26 @@ function findprod()
 {
 	_.each(shop,function(item){
 		var found=_.filter(log,function(logitem){
-			return each.name===logitem.name;
-		})
-		if(!!found)
+
+			if(logitem.name===item.name)
+			{
+				var match = logitem.name;
+			}
+		if(!!match)
 		{
-			_.each(found,function(index){
-			fp.push(index);
-			});
+			fp.push({id:logitem.id, name:logitem.name, qty:item.qty});
+			
 		}
+		
 
 	});
+});
+	for(i=0;i<(log.length);i++)
+		{
+			document.write(fp[i].id+"&nbsp");
+			document.write(fp[i].name+"&nbsp");
+			document.write(fp[i].qty+"<br>");
+		}
 }
 
 
